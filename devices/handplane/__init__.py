49200c370525e0cdc6395b5b50cd089c27b52f70
AquaticNightmare <troubledbass@tutanota.com>
2020-09-15 18:09:56 +0200

    changed tif to tiff in handplane settinhgs

--- devices/handplane/__init__.py
+++ devices/handplane/__init__.py
@@ -180,7 +180,7 @@ class EZB_Device_Handplane(bpy.types.PropertyGroup, EZB_Device):
         items=[
             ('TGA', 'TGA', 'Export images as .tga'),
             ('PNG', 'PNG', 'Export images as .png'),
-            ('TIF', 'TIF', 'Export images as .tif'),
+            ('TIF', 'TIFF', 'Export images as .tif'),
         ],
         default='PNG',
         name='Format'
